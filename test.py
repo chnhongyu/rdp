74e038e506cf27cc729f75004b711c7795b3c057
chnhongyu <503336@student.fontys.nl>
2023-09-15 22:01:31 +0200

    gelukt met installeren

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ all_sheets = pd.read_excel('Running Dinner dataset 2022.xlsx',sheet_name=None)
 for sheet_name, sheet_df in all_sheets.items():
     print(f"sheet_name {sheet_name} data：")
     print(sheet_df)
+
+print()
