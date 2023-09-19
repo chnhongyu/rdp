2a0c1ca13e4b596ca41df157f4e88900c44827a6
TiesVereijken <508276@fontys.nl>
2023-09-19 13:20:25 +0200

    Datasets ingeladen

--- test.py
+++ test.py
@@ -3,9 +3,20 @@ import pandas as pd
 # import sys
 # print(sys.executable)
 
-all_sheets = pd.read_excel('Running Dinner dataset 2022.xlsx',sheet_name=None)
-for sheet_name, sheet_df in all_sheets.items():
-    print(f"sheet_name {sheet_name} data：")
-    print(sheet_df)
+Dataset = 'Running Dinner dataset 2021.xlsx'
+Bewoners = pd.read_excel(Dataset, sheet_name = 'Bewoners')
+Adressen = pd.read_excel(Dataset, sheet_name = 'Adressen')
+Paar = pd.read_excel(Dataset, sheet_name = 'Paar blijft bij elkaar', header = 1)
+Buren = pd.read_excel(Dataset, sheet_name = 'Buren', header = 1)
+Kookte = pd.read_excel(Dataset, sheet_name = 'Kookte vorig jaar', header = 1)
+Tafelgenoot = pd.read_excel(Dataset, sheet_name = 'Tafelgenoot vorig jaar', header = 1)
 
-print()
+# verzamelingen
+#A = all_sheets['Adressen']['Huisadres']
+#D = all_sheets['Bewoners']['Bewoner']
+#E = all_sheets['Paar blijft bij elkaar'][['Bewoner1', 'Bewoner2']]
+#G = list('Voor', 'Hoofd', 'Na')
+
+# parameters
+#La = all_sheets['Adressen'][['Huisadres', 'Min groepsgrootte']]
+#Ua = all_sheets['Adressen'][['Huisadres', 'Max groepsgrootte']]
