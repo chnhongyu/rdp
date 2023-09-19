1e86a57a5ebcb2545fd11a25690e279f18ee5ec9
chnhongyu <503336@student.fontys.nl>
2023-09-19 21:53:01 +0200

    dictionary

---
+++ dataunderstanding.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#inlezen input
+Data = 'Running Dinner eerste oplossing 2021.xlsx'
+df = pd.read_excel(Data)
+
+kook_dict = {"Voor": [], "Hoofd": [], "Na": []}
+
+for index, row in df.iterrows():
+    huisadres = row["Huisadres"]
+    kookt = row["kookt"]
+    
+    # Voeg het huisadres toe aan de juiste kookvoorkeur
+    if kookt in kook_dict:
+        kook_dict[kookt].append(huisadres)
+
+print(kook_dict)
