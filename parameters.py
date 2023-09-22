ee79b5449ed88680ce406a25f7fe0693721e3278
chnhongyu <503336@student.fontys.nl>
2023-09-22 13:34:40 +0200

    constraint1

--- parameters.py
+++ parameters.py
@@ -35,6 +35,22 @@ c1 = {}
 d = set(oplossing['Bewoner'])
 a = set(oplossing['Huisadres'])
 g = set(oplossing['kookt'])
-for i in g:
-    print(i)
 
+for i in d:
+    abc= oplossing[oplossing['Bewoner']==i] [['Voor','Hoofd','Na']].values[0]
+    l = len(set(abc))
+    c1[i]= l 
+#c1 returnt een dictionary, met 'bewoner': aantal verschillende adressen op de avond
+# wat dus gelijk moet zijn aan 3, voor hoofd en na
+# set() maakt het uniek
+
+dfc1 = pd.DataFrame(list(c1.items()), columns=['Sleutel', 'Waarde'])
+
+ongelijke_waarden = dfc1[dfc1['Waarde'] != 3]
+
+# Als de DataFrame 'ongelijke_waarden' leeg is, betekent dit dat er geen 'Sleutel' is met een andere 'Waarde' dan 3
+if ongelijke_waarden.empty:
+    print("Alle 'Waarde'-waarden zijn gelijk aan 3.")
+else:
+    print("Er zijn 'Sleutel'-waarden met een andere 'Waarde' dan 3:")
+    print(ongelijke_waarden)
