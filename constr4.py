a81d4c1d82b9d0d20adf31328be8832afafeb53c
chnhongyu <503336@student.fontys.nl>
2023-09-24 23:24:28 +0200

    c4

--- constr4.py
+++ constr4.py
@@ -6,16 +6,20 @@ Dataset1 = 'Running Dinner dataset 2021.xlsx'
 Dataset2 = 'Running Dinner eerste oplossing 2021.xlsx'
 oplossing = pd.read_excel(Dataset2)
 
-Bewoners = pd.read_excel(Dataset1, sheet_name = 'Bewoners')
-
+Adressen = pd.read_excel(Dataset1, sheet_name = 'Adressen')
+lb = Adressen[['Huisadres','Min groepsgrootte']]
+ub = Adressen[['Huisadres','Max groepsgrootte']]
+print(lb)
 # daadwerkelijke aantal vergelijken met toegelaten aantal, voor elk adres
 
 adres = set(oplossing['Huisadres'])
 # print(adres)
 
 con4 = []
-for a in adres:
-    con4.append(oplossing[oplossing['Huisadres']==a][['Huisadres','aantal']])
+# for a in adres:
+#     if oplossing[oplossing['Huisadres']==a]['aantal']> lb[lb['Huisadres']==a] and oplossing[oplossing['Huisadres']==a]['aantal']<ub :
+#         con4.append(oplossing[oplossing['Huisadres']==a][['Huisadres','aantal']])
 # print(con4)
 # con4 returnt huisadres en aantal
-print(oplossing[oplossing['Huisadres']=='V8'][['Huisadres','aantal']])
+print()
+
