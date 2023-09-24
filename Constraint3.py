71ac30e512daf628216b2c48d29a9fdb18f12bdd
chnhongyu <503336@student.fontys.nl>
2023-09-24 21:54:34 +0200

    .

--- Constraint3.py
+++ Constraint3.py
@@ -22,8 +22,21 @@ oplossing = pd.read_excel(Dataset2)
 #ik loop nu tegen aan NaN values
 # eerst mensen filteren die niet hoef te koken
 
+#nog een check met de constr dataset
+Dataset = 'Running Dinner dataset 2021.xlsx'
+Bewoners = pd.read_excel(Dataset, sheet_name = 'Bewoners')
+
+
 niet_kokend = oplossing[oplossing['kookt'].isna()]
 kokend = oplossing.dropna(subset=['kookt'])
+
+niet_kokend_check = set(Bewoners[Bewoners['Kookt niet']==1]['Huisadres'])
+oplos_niet_kokend_check = set(niet_kokend['Huisadres'])
+if oplos_niet_kokend_check == niet_kokend_check:
+    print('planning voldoet aan de data')
+else:
+    print('niet kokende personen komt niet overeen met de data')
+
 
 con3 = []
 for i in range(len(kokend)):
@@ -33,5 +46,7 @@ for i in range(len(kokend)):
     if lhs!=rhs:
         con3.append(kokend.iloc[i]['Bewoner'])
         print('Deze bewoners koken op een andere adres terwijl zij thuis moeten koken')
+if len(con3)==0:
+    print('voldoet constr 3')
 
-#als con3 0 is voldoet het aan constraint 3
+
--- constr4.py
+++ constr4.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
 #inlezen input
+Dataset1 = 'Running Dinner dataset 2021.xlsx'
+
 Dataset2 = 'Running Dinner eerste oplossing 2021.xlsx'
 oplossing = pd.read_excel(Dataset2)
+
+Bewoners = pd.read_excel(Dataset1, sheet_name = 'Bewoners')
 
 # daadwerkelijke aantal vergelijken met toegelaten aantal, voor elk adres
 
