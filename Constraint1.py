f8f623f46543800440d41b6cd6e59778fcb3744f
chnhongyu <503336@student.fontys.nl>
2023-09-25 11:52:29 +0200

    asdf

---
+++ Constraint1.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+import pandas as pd 
+from Constraint5 import Constraint5
+
+Oplossing2021Excel = 'Running Dinner eerste oplossing 2021.xlsx'
+Oplossing2021 = pd.read_excel(Oplossing2021Excel)
+
+Dataset2021Excel = 'Running Dinner dataset 2021.xlsx'
+Bewoners2021 = pd.read_excel(Dataset2021Excel, sheet_name = 'Bewoners')
+Adressen2021 = pd.read_excel(Dataset2021Excel, sheet_name = 'Adressen')
+Paar2021 = pd.read_excel(Dataset2021Excel, sheet_name = 'Paar blijft bij elkaar', header = 1)
+Buren2021 = pd.read_excel(Dataset2021Excel, sheet_name = 'Buren', header = 1)
+Kookte2021 = pd.read_excel(Dataset2021Excel, sheet_name = 'Kookte vorig jaar', header = 1)
+Tafelgenoot2021 = pd.read_excel(Dataset2021Excel, sheet_name = 'Tafelgenoot vorig jaar', header = 1)
+
+#-----------------------------
+# Oplossing2022Excel = 'Running Dinner eerste oplossing 2022.xlsx'
+# Oplossing2022 = pd.read_excel(Oplossing2022Excel)
+
+# Dataset2022Excel = 'Running Dinner dataset 2022.xlsx'
+# Bewoners2022 = pd.read_excel(Dataset2022Excel, sheet_name = 'Bewoners')
+# Adressen2022 = pd.read_excel(Dataset2021Excel, sheet_name = 'Adressen')
+# Paar2022 = pd.read_excel(Dataset2022Excel, sheet_name = 'Paar blijft bij elkaar', header = 1)
+# Buren2022 = pd.read_excel(Dataset2022Excel, sheet_name = 'Buren', header = 1)
+# Kookte2022 = pd.read_excel(Dataset2022Excel, sheet_name = 'Kookte vorig jaar', header = 1)
+# Tafelgenoot2022 = pd.read_excel(Dataset2022Excel, sheet_name = 'Tafelgenoot vorig jaar', header = 1)
+
+#------------------------------
+# Constraint 1 zorgt voor dat elke deelnemer, elke gang, op 1 adres eet.
+# dus deelnemer1: | a1 | a2 | a3 |
+# input: Oplossing2021
+# output: voldoet wel/niet aan constraint1
+
