b6557243c22d107c69c12f757a8257bee078a5d1
chnhongyu <503336@student.fontys.nl>
2023-09-27 16:14:47 +0200

    .

--- C2.py
+++ C2.py
@@ -5,4 +5,15 @@ oplossing = oplossing = pd.read_excel('Running Dinner eerste oplossing 2021.xlsx
 #constraint 2 kijkt voor elk adres, wat onder kookt staat gelijk moet zijn aan elkaar
 
 set_adres = set(oplossing['Huisadres'])
-print(set_adres)
+
+print(len(set(oplossing[oplossing['Huisadres']=='V12' ]['kookt'].values)))
+
+
+check = []
+statement = True
+
+for adres in set_adres:
+    if ( len( set( oplossing[ oplossing['Huisadres']==adres ]['kookt'].values ) )) != 1:
+        check.append(adres)
+        statement = False
+print(statement)
