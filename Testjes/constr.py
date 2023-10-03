767e0288d6deac66a3998797f021128b874407fd
TiesVereijken <508276@fontys.nl>
2023-10-03 13:25:33 +0200

    ajfka

--- Testjes/constr.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-Oplossing = pd.read_excel('Running Dinner eerste oplossing 2021.xlsx')
-
-# constr 1, for elke deelnemer
-# con 2, for elke adres
-# con 3, for elke kokend adres
-# con 4, for 
